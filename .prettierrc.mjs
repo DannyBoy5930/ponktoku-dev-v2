46bfe3b5f1331d9063fb5c8e1e166280a6d4d6a8
Danny Bao <88077806+DannyBoy5930@users.noreply.github.com>
2024-08-07 19:59:46 +1000

    Add prettier plugins

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,11 @@
-/** @type {import("prettier").Config} */
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
 export default {
-  plugins: ["prettier-plugin-astro"],
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
   overrides: [
     {
       files: "*.astro",
