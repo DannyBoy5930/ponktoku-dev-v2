c6a637a0d106dbc13d738ccef1096561ecdd3914
Danny Bao <88077806+DannyBoy5930@users.noreply.github.com>
2024-08-09 21:13:06 +1000

    feat: Subscribe to global state
     - Implement logic for system theme
     - Run the initThemeStoreSubscribe logic onMount through nanostore essentially on page load

--- src/lib/stores/theme.ts
+++ src/lib/stores/theme.ts
@@ -1,4 +1,5 @@
 import { persistentAtom } from "@nanostores/persistent";
+import { onMount } from "nanostores";
 
 export const THEME_MAP = {
   light: "light",
@@ -15,3 +16,40 @@ export const themeStore = persistentAtom<ThemeValue>(
   STORAGE_THEME_KEY,
   THEME_MAP.system,
 );
+
+// Subscribing to Global State
+const initThemeStoreSubscribe = () => {
+  const applyTheme = (theme: ThemeValue) => {
+    if (theme === THEME_MAP.dark) {
+      document.documentElement.classList.add("dark");
+    } else if (theme === THEME_MAP.light) {
+      document.documentElement.classList.remove("dark");
+    }
+  };
+
+  const handleMediaQuery = (query: { matches: boolean }) => {
+    applyTheme(query.matches ? "dark" : "light");
+  };
+
+  themeStore.subscribe((theme) => {
+    // When in 'system' state apply mode according to user's device theme
+    if (theme !== THEME_MAP.system) {
+      applyTheme(theme);
+      return;
+    }
+
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    // Make sure the EventListener is not registered redundantly by removing it before registering it.
+    mediaQuery.removeEventListener("change", handleMediaQuery);
+    mediaQuery.addEventListener("change", handleMediaQuery);
+    handleMediaQuery(mediaQuery);
+  });
+};
+
+// Don't run it in the SSR
+if (typeof window !== "undefined") {
+  onMount(themeStore, () => {
+    initThemeStoreSubscribe();
+  });
+}
